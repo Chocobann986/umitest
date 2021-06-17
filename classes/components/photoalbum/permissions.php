10df42e88215ef8bf3c65bf67ce51fa2026dca7b
Антон Наталич <vinchesterpc@gmail.com>
2021-06-17 05:03:56 +0300

    added clean UMI CMS

---
+++ classes/components/photoalbum/permissions.php
@@ -0,0 +1,35 @@
+<?php
+
+	/** Группы прав на функционал модуля */
+	$permissions = [
+		/** Права на просмотр фотогелерей */
+		'albums' => [
+			'album',
+			'albums',
+			'photo'
+		],
+		/** Права на администрирование модуля */
+		'albums_list' => [
+			'lists',
+			'add',
+			'edit',
+			'activity',
+			'uploadimages',
+			'upload_arhive',
+			'album.edit',
+			'photo.edit',
+			'publish',
+			'getactivechildrenpart',
+			'getinactivechildrenpart',
+			'copy',
+		],
+		/** Права на работу с настройками */
+		'config' => [
+			'config'
+		],
+		/** Права на удаление фотографий и альбомов */
+		'delete' => [
+			'getchildrenpart',
+			'del'
+		]
+	];
