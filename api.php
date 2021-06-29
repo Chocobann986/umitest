71e105a210831843aabe8ef4127bce8ac3b84cd9
Антон Наталич <vinchesterpc@gmail.com>
2021-06-29 15:03:39 +0300

    Доделано API

--- api.php
+++ api.php
@@ -11,7 +11,7 @@ function apiInit()
     $method = $_REQUEST && $_REQUEST['method']
         ? $_REQUEST['method']
         : null;
-        
+
     switch ($method) {
         case 'test':
             return test();
@@ -19,6 +19,18 @@ function apiInit()
 
         case 'getTickets':
             return getTickets(getRequest('page'));
+            break;
+        case 'getTicketById':
+            return getTicketById(getRequest('id'));
+            break;
+        case 'deleteTicket':
+            return deleteTicket(getRequest('id'));
+            break;
+        case 'editTicket':
+            return editTicket(getRequest('id'), getRequest('name'),getRequest('malfunction'), getRequest('description'), getRequest('photo'), getRequest('acceptance_date'), getRequest('status'));
+            break;
+        case 'createTicket':
+            return createTicket(getRequest('name'), getRequest('malfunction'), getRequest('description'), getRequest('photo'), getRequest('acceptance_date'), getRequest('status'));
         default:
             header("Status: 400 Bad Request");
             return ['error' => 'Unknown method'];
@@ -33,7 +45,8 @@ function test()
     ];
 }
 
-function getPhotoURL($photoURL){
+function getPhotoURL($photoURL)
+{
     $domainsCollection = domainsCollection::getInstance()->getDefaultDomain();
     $domain = $domainsCollection->getHost();
     $protocol = $domainsCollection->getProtocol();
@@ -41,23 +54,115 @@ function getPhotoURL($photoURL){
     return $protocol . "://" . $domain . $photoURL;
 }
 
-function getTickets($page = 1){
+function getTickets($page = 1)
+{
     $limit = 2;
     $sel = new selector('objects');
     $sel->types('object-type')->guid('repair-ticket');
     $offset = $limit * ($page - 1);
     $sel->limit($offset, $limit);
-    $result = [];
-    foreach ($sel->result() as $ticket){
-        $photo = $ticket->getValue('photo');
-        $result[] = [
-            'name' => $ticket->getName(),
-            'malfunction' =>$ticket->getValue('malfunction'),
-            'description' => $ticket->getValue('description'),
-            'photo' => $photo ? getPhotoURL($photo->getFilePath(true)) : null,
-            'acceptanceDate' => $ticket->getValue('acceptance_date'),
-            'status' => $ticket->getValue('status')
+
+    return getTicketFromSelector($sel);
+}
+
+function getTicketById($id)
+{
+    if ($id) {
+        $sel = new selector('objects');
+        $sel->types('object-type')->guid('repair-ticket');
+        $sel->where('id')->equals($id);
+        return getTicketFromSelector($sel);
+    }
+    header("Status: 400 Bad Request");
+    return [
+        'error' => 'Id not specified',
+        'id' => $id
+    ];
+}
+
+function getTicketFromSelector($sel)
+{
+    if ($sel->length() > 0) {
+        $result = [];
+        foreach ($sel->result() as $ticket) {
+            $photo = $ticket->getValue('photo');
+            $result[] = [
+                'name' => $ticket->getName(),
+                'malfunction' => $ticket->getValue('malfunction'),
+                'description' => $ticket->getValue('description'),
+                'photo' => $photo ? getPhotoURL($photo->getFilePath(true)) : null,
+                'acceptanceDate' => $ticket->getValue('acceptance_date'),
+                'status' => $ticket->getValue('status')
+            ];
+        }
+        return $result;
+    } else {
+        header("Status: 404 Not Found");
+        return [
+            'error' => 'Not Found'
         ];
     }
-    return $result;
+}
+
+function deleteTicket($object_id)
+{
+    $objectsCollection = umiObjectsCollection::getInstance();
+    $event = new umiEventPoint('systemDeleteObject');
+    $event->addRef('object', $object);
+    $event->setMode('before');
+    $event->call();
+
+    $result = $objectsCollection->delObject($object_id);
+
+    $event->setMode('after');
+    $event->call();
+
+    if ($result) {
+        header("Status: 200 Ok");
+        return [
+            'deletedId' => $object_id
+        ];
+    } else {
+        header("Status: 400 Bad Request");
+        return [
+            'error' => "data with this id don't exists."
+        ];
+    }
+}
+
+function createTicket($name, $malfaction, $description = false, $photo = false, $acceptance_date = false, $status)
+{
+    $umiObjectsTypes = umiObjectTypesCollection::getInstance();
+    $typeId = $umiObjectsTypes->getTypeIdByGUID('repair-ticket');
+
+    $objectsCollection = umiObjectsCollection::getInstance();
+    $objectId = $objectsCollection->addObject($name, $typeId);
+    $object = $objectsCollection->getObject($objectId);
+
+    $object->setValue('nazvanie', $object->getName());
+    $object->setValue('malfunction', $malfaction);
+    $object->setValue('description', $description);
+    $object->setValue('photo', $photo);
+    $object->setValue('acceptance_date', $acceptance_date);
+    $object->setValue('status', $status);
+    $object->commit();
+
+    return $object->getId();
+}
+
+function editTicket($id, $name, $malfunction, $description = false, $photo = false, $acceptance_date = false, $status)
+{
+    $objectsCollection = umiObjectsCollection::getInstance();
+    $object = $objectsCollection->getObject($id);
+
+
+    $object->setName($name);
+    $object->setValue('malfunction', $malfunction);
+    $object->setValue('description', $description);
+    $object->setValue('photo', $photo);
+    $object->setValue('acceptance_date', $acceptance_date);
+    $object->setValue('status', $status);
+    $object->commit();
+
+    return $object->getId();
 }
